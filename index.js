20285d7cc7a5e32b127763d2d1831d289721d872
Sam Serrels <doogly1@gmail.com>
2019-04-06 16:54:49 +0100

    Workign on linking

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 let fs = require('fs');
 let path = require("path");
+
+//simple non-pop() pop.
+const getStackTop = (stack) => stack[stack.length - 1];
+//As above but with empty throw
+const getStackTopS = (stack) => {
+  const a = getStackTop(stack);
+  if (!a) {
+    throw ("Stack Empty!");
+  } return a;
+};
+
+//Starts with anything from this array
+const startsWithAny = (e, list) => list.find((i) => e.toLowerCase().startsWith(i.toLowerCase()));
+//Starts with anything from any of these array
+const startsWithAnyAny = (e, lists) => {
+  for (list of lists) {
+    let a = startsWithAny(e, list);
+    if (a) { return a; }
+  }
+  return undefined;
+}
+
 var walkSync = function (dir, fl, d) {
   const files = fs.readdirSync(dir);
   let fileList = fl || [];
@@ -57,26 +79,8 @@ async function Parse(VPC_FILE) {
   const sliceFirst = (s) => s.indexOf(' ') === -1 ? "" : s.substr(s.indexOf(' ') + 1);
   //Trims Dollar Sign if it's there.
   const trimKey = (key) => key.startsWith('$') ? key.substr(1) : key;
-  //simple non-pop() pop.
-  const getStackTop = (stack) => stack[stack.length - 1];
-  //As above but with empty throw
-  const getStackTopS = (stack) => {
-    const a = getStackTop(stack);
-    if (!a) {
-      throw ("Stack Empty!");
-    } return a;
-  };
-  //Starts with anything from this array
-  const startsWithAny = (e, list) => list.find((i) => e.toLowerCase().startsWith(i.toLowerCase()));
-  //Starts with anything from any of these array
-  const startsWithAnyAny = (e, lists) => {
-    for (list of lists) {
-      let a = startsWithAny(e, list);
-      if (a) { return a; }
-    }
-    return undefined;
-  }
- //-----------------
+
+  //-----------------
 
   const custombuildstepFlags = ["$description", "$commandline", "$outputs"];
   const configGroups = ["$general", "$compiler", "$linker", "$librarian", "$custombuildstep", "$prebuildevent", "$prelinkevent", "$postbuildevent", "$debugging", "$manifesttool", "$xmldocumentgenerator", "$browseinformation", "$resources"];
@@ -94,7 +98,7 @@ async function Parse(VPC_FILE) {
   const configGeneralFlags = ["$TargetExtension", "$TargetName", "$outputdirectory", "$intermediatedirectory", "$ExtensionsToDeleteOnClean", "$BuildLogFile", "$InheritedProjectPropertySheets", "$ConfigurationType", "$UseOfMFC", "$UseOfATL", "$MinimizeCRTUseInATL", "$CharacterSet", "$CommonLanguageRuntimeSupport", "$WholeProgramOptimization", "$PlatformToolset", "$ExecutableDirectories", "$GameOutputFile"];
   const configDebuggingFlags = ["$Command", "$CommandArguments", "$WorkingDirectory", "$Attach", "$DebuggerType", "$Environment", "$MergeEnvironment", "$SQLDebugging"];
   const configChildren = [custombuildstepFlags, configCompilerFlags, configLibrarianFlags, configLinkerFlags, configGeneralFlags, configDebuggingFlags, configPreBuildeventFlags, configPreLinkeventFlags, configPostBuildeventFlags, configManifesttoolFlags, configResourcesFlags, configXmldocumentgeneratorFlags, configBrowseInformationFlags,];
-  
+
   //Some things may open scope after, or may not. 
   //Setting this to true indicates that if the next token isn't a scope operator, to remove the thing at the top of the stack.
   let possibleScope = false;
@@ -171,7 +175,7 @@ async function Parse(VPC_FILE) {
       cfg_child[trimKey(a)].push(sliceFirst(v));
       //----------------------------------------------
     } else if (v.startsWith("$project")) {
-      let proj = { folders: [], files: [], shaders: [] };
+      let proj = { folders: [], files: [], shaders: [], data: sliceFirst(v) };
       vpco.project = proj;
       stack.push(proj);
     } else if (v.startsWith("$folder")) {
@@ -242,6 +246,160 @@ async function Parse(VPC_FILE) {
 }
 
 
+function Link(VPCOs) {
+  //Suppress warnings for:
+  let missingVPCOs = [
+    "source_ppu_prx_ps3_base.vpc",
+    "source_lib_x360_base.vpc",
+    "source_ppu_lib_ps3_base.vpc"];
+
+  const findV = (fn) => VPCOs.find((v) => v.f == fn);
+  const deMacro = (vcpo, str) => {
+    for (m in vcpo.macros) {
+      str = str.replace('$' + m, vcpo.macros[m]);
+    }
+    return str;
+  };
+  //Assume only vpc's with $Project "Something" are Targets
+  let targets = VPCOs.filter((vpco) => {
+    return vpco.project && vpco.project.data !== "";
+  });
+
+  console.log(targets.length, "Targets Found");
+
+
+  let getIncludes = (VPCO, parentChain) => {
+    parentChain = parentChain || [];
+    return VPCO.includes.map((i) => {
+      let di = deMacro(VPCO, i);
+      let dip = path.resolve(path.dirname(VPCO.f), di);
+      let ivpco = findV(dip);
+
+      if (missingVPCOs.find((v) => v === path.basename(di))) {
+        return { resolvedPath: dip, data: undefined, rawPath: i };
+      }
+
+      if (!ivpco) {
+        //Try resolving macros by looking up the parent chain.
+        let macroContext = { macros: {} }
+        for (let idx = parentChain.length; idx >= 1; idx--) {
+          const p = parentChain[idx - 1];
+          for (m in p.macros) {
+            if (macroContext.macros[m] !== undefined && macroContext.macros[m] !== p.macros[m]) {
+              console.warn("Macro redefinition in include chain:", m, "-", macroContext.macros[m], "-", p.macros[m]);
+            }
+            macroContext.macros[m] = p.macros[m];
+          }
+          //attempt resolve
+          di = deMacro(macroContext, i);
+          dip = path.resolve(path.dirname(VPCO.f), di);
+          ivpco = findV(dip);
+          VPCO.macroChain = macroContext;
+          if (ivpco) {
+            //  console.log("YAY!", idx, path.basename(dip));
+            break;
+          }
+        }
+      }
+      if (!ivpco) {
+        //try hack
+        let findThisFolder = path.basename(path.dirname(di));
+        let anotherFileHere = VPCOs.find((v) => path.basename(path.dirname(v.f)) == findThisFolder);
+
+        if (anotherFileHere) {
+          let newpath = path.dirname(anotherFileHere.f);
+          //But is the file here?
+          if (findV(newpath + path.sep + path.basename(di))) {
+            dip = newpath + path.sep + path.basename(di);
+            ivpco = findV(dip);
+            //console.warn("Using search hack to locate ", di, newpath);
+          }
+        }
+      }
+
+      if (ivpco) {
+        parentChain.push(VPCO);
+        //console.log("Including ", path.basename(dip));
+        //console.log(" ".repeat(parentChain.length)+"including",path.basename(dip));
+        //  console.log("Including ", path.basename(dip), "#", parentChain.reduce((p, c, i) => p + "\n" + " ".repeat(i) + "-> " + path.basename(c.f), ""));
+        ivpco.includesVCPO = ivpco.includesVCPO || [];
+        //recurse down
+        ivpco.includesVCPO = getIncludes(ivpco, parentChain);
+        return { resolvedPath: dip, data: ivpco, rawPath: i };
+      } else {
+        console.warn("Can't find include " + dip + "\n RawString:" + i + "\n From File " + VPCO.f);
+        return { resolvedPath: dip, data: undefined, rawPath: i };;
+      }
+    }
+
+    );
+  }
+
+  //Will recurse down B, merging into A. B tree shouldn't change.
+  let DeepMerge = (a, b) => {
+    if (!b) {
+      a.includesVCPO.forEach(ip => {
+        if (ip.data === undefined) { return; }
+        DeepMerge(a, ip.data);
+      });
+    } else {
+      b.includesVCPO.forEach(ip => {
+        if (ip.data === undefined) { return; }
+        DeepMerge2(a, ip.data);
+      });
+    }
+  }
+
+  function GetMeANewOfThisType(a) {
+    if (typeof (a) === 'object') {
+      if (Array.isArray(a)) {
+        return [];
+      } else {
+        return {};
+      }
+    }
+    return 0;
+  }
+  function DeepMerge2(a, b) {
+    if (typeof (b) === 'object') {
+      if (Array.isArray(b)) {
+        return a.concat(b.map((v) => DeepMerge2(a, v)));
+      } else {
+        for (let key in b) {
+          if (a[key] === undefined) {
+            a[key] = GetMeANewOfThisType(b[key]);
+          }else if(typeof a[key] !== 'object'){
+            //Already existing primative, skip
+            if(a[key] !== b[key] && key !== 'f'){
+              console.warn("include flag overwrite: ", key,"-", a[key], b[key]);
+            }
+            continue;
+          }
+          a[key] = DeepMerge2(a[key], b[key]);
+        }
+        return a;
+      }
+    }
+    return b;
+  };
+
+  //Link Includes
+  targets.forEach((t) => {
+    try {
+      t.includesVCPO = getIncludes(t);
+      console.log("---\nLink done for ", t.f);
+      DeepMerge(t);
+      console.log("---\nMErge done for ", t.f);
+    } catch (error) {
+      console.warn("Link error", t.f);
+      console.warn(error);
+    }
+  });
+  console.log("Link done");
+
+
+}
+
 
 async function Main() {
   const ROOT_DIR = path.resolve(process.argv[2]) + path.sep;
@@ -258,7 +416,7 @@ async function Main() {
 
   let vpos = await Promise.all(VPC_FILE_LIST.map(Parse));
 
-  console.log(JSON.stringify(vpos));
+  //console.log(JSON.stringify(vpos));
   console.info("Parsing Done, Files:", vpos.length);
   if (Object.keys(MissingkeywordMap).length !== 0 || MissingkeywordMap.constructor !== Object) {
     let keysSorted = Object.keys(MissingkeywordMap).sort(function (a, b) { return MissingkeywordMap[b] - MissingkeywordMap[a] })
@@ -268,6 +426,8 @@ async function Main() {
       console.log(keysSorted[index], MissingkeywordMap[keysSorted[index]]);
     }
   }
+
+  Link(vpos);
 }
 
 
